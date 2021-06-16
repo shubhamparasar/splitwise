9e11192759af8422e6f062d5892cdf7a46d0e5d1
shubhamparasar <shubhamparasar.40@gmail.com>
2021-06-17 04:51:59 +0530

    expense sharing application

--- src/com/splitwise/main.java
+++ src/com/splitwise/main.java
@@ -1,12 +1,45 @@
 package com.splitwise;
 
+import com.splitwise.controllers.ExpenseController;
+import com.splitwise.controllers.UserController;
+import com.splitwise.repositories.ExpenseRepository;
+import com.splitwise.repositories.GroupRepository;
 import com.splitwise.repositories.userRepository;
+import com.splitwise.services.authentication.DefaultPasswordEncoder;
+import com.splitwise.services.authentication.PasswordEncoder;
+import com.splitwise.services.authentication.console.commands.ExitCommand;
+import com.splitwise.services.authentication.console.commands.RegisterUserCommand;
+import com.splitwise.services.authentication.console.handler.CommandHandler;
+import com.splitwise.services.authentication.console.handler.ICommandHandler;
+import lombok.Getter;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+
 
 public class main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        Splitwise splitwise= new Splitwise();
 
+        ICommandHandler commandHandler=new CommandHandler();
+        commandHandler.registerCommand(new RegisterUserCommand(splitwise));
+        commandHandler.registerCommand(new ExitCommand());
 
-        //Write code here
+        while (true){
+            System.out.println("\n> ");
+            List<String> input= Arrays.asList(br.readLine().split(" "));
+            try{
+                commandHandler.execute(input);
+            }
+            catch (Exception e){
+                 System.out.println(e.getClass().getSimpleName() + ":" + e.getMessage());
+                 System.out.println(Arrays.toString(e.getStackTrace()));
+            }
+        }
     }
 }
--- src/com/splitwise/services/authentication/DefaultPasswordEncoder.java
+++ src/com/splitwise/services/authentication/DefaultPasswordEncoder.java
@@ -1,6 +1,21 @@
 package com.splitwise.services.authentication;
 
 public class DefaultPasswordEncoder implements PasswordEncoder{
+
+    private static DefaultPasswordEncoder INSTANCE;
+    private DefaultPasswordEncoder(){};
+    public static DefaultPasswordEncoder getINSTANCE(){
+        if(INSTANCE==null) {
+            synchronized (DefaultPasswordEncoder.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = new DefaultPasswordEncoder();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+
     @Override
     public String encode(String password, String username) {
         return password;
